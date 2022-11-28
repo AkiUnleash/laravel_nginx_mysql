ac002ed17c54caa8f91869942ed6c3b1305f8eb2
Akio Yano <akio.yano@unleash-jp.com>
2022-11-28 13:32:15 +0900

    Fixed.

---
+++ docker/app/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd /app
+
+php artisan config:cache
+php artisan view:cache
+php artisan route:cache
+php artisan migrate --force
+composer install --optimize-autoloader --no-dev
+npm run build
