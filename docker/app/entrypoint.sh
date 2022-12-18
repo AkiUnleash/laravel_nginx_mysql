819fa9a514df485d10f710c90d3158bf6c29f4ec
Akio Yano <akio.yano@unleash-jp.com>
2022-12-18 11:48:42 +0900

    Create buildpec.yml.

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -5,6 +5,8 @@ cd /app
 php artisan config:cache
 php artisan view:cache
 php artisan route:cache
-php artisan migrate --force
+# php artisan migrate --force
 composer install --optimize-autoloader --no-dev
 npm run build
+
+. /usr/local/bin/docker-php-entrypoint php-fpm
